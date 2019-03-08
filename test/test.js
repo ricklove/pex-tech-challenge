869a49a3920574e18a2c233f1916a7658264097a
Rick Love <rick@toldpro.com>
2019-03-07 18:11:51 -0600

    Added implemented functions to the test file per requirements

--- test/test.js
+++ test/test.js
@@ -1,11 +1,59 @@
 const Immutable = require('immutable');
 const assert = require('assert');
-const transformErrors = require('../src/transform-errors');
 
-// Example Function
-// function transformErrors() {
-//   return Immutable.Map();
-// }
+// Added implemented functions to this file as instructed by spec.md
+// const transformErrors = require('../src/transform-errors');
+
+function isMap(obj) {
+  return obj && obj.constructor && obj.constructor.name === 'Map';
+}
+
+function isList(obj) {
+  return obj && obj.constructor && obj.constructor.name === 'List';
+}
+
+function isStringList(obj) {
+  return isList(obj) && !obj.toArray().filter(x => typeof x !== 'string').length;
+}
+
+function distinct(items) {
+  return items
+    .filter(x => x)
+    .reduce((out, x) => { if (out.indexOf(x) < 0) { out.push(x); return out; } else { return out; } }, []);
+}
+
+function concat(arrays) {
+  return arrays
+    .reduce((out, x) => out = out.concat(x), []);
+}
+
+function joinStringWithPeriod(items) {
+  return !items.length ? ''
+    : distinct(items).join('. ') + '.';
+}
+
+function flattenToStringArray(obj) {
+  return isMap(obj) ? concat(obj.keySeq().toArray().map(k => flattenToStringArray(obj.get(k))))
+    : isList(obj) ? concat(obj.toArray().map(x => flattenToStringArray(x)))
+      : [obj];
+}
+
+function flattenToStringArray_top(obj) {
+  return joinStringWithPeriod(flattenToStringArray(obj));
+}
+
+function flattenOnlyStringListArray(obj) {
+  return isMap(obj) ? Immutable.Map(obj.keySeq().toArray().map(k => [k, flattenOnlyStringListArray(obj.get(k))]))
+    : isStringList(obj) ? joinStringWithPeriod(obj.toArray())
+      : isList(obj) ? Immutable.List(obj.toArray().map(x => flattenOnlyStringListArray(x)))
+        : obj;
+}
+
+function transformErrors(obj, keepFields) {
+  return Immutable.Map(obj.keySeq().toArray().map(k =>
+    [k, keepFields.indexOf(k) >= 0 ? flattenOnlyStringListArray(obj.get(k)) : flattenToStringArray_top(obj.get(k))]
+  ));
+}
 
 it('should tranform errors', () => {
   // example error object returned from API converted to Immutable.Map
